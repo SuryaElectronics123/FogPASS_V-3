806bde7d2fa3eebdd2c9387b9905d61bfd747caa
Sai <vigneswar.andavarapu@gmail.com>
2025-05-03 10:55:44 +0530

    update upload max limit

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ getPrivateKeyVal().then(res => {
     privateKeyVal = res;
 })
 
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
 app.use(cors())
 app.get('/health-check', (req, res) => {
     res.send('success');
